4534a6af51b3a1df6fd7c74899c577755d45d13f
Peter van der Wal <peterzakelijk@live.nl>
2024-05-05 14:16:48 +0200

    UI into containers

--- src/main/java/pwal/org/UI.java
+++ src/main/java/pwal/org/UI.java
@@ -17,12 +17,17 @@ public class UI {
         // Set the size of the JFrame
         frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(0, 1));
 
+        Container inputContainer = new Container();
+        inputContainer.setLayout(new GridLayout(0,1));
+        inputContainer.add(new JLabel("hello"));
+        frame.add(inputContainer);
 
-        Container container = frame.getContentPane();
-        container.setLayout(new GridLayout(0,3));
-
-        this.generateNumpadButtons().forEach(container::add);
+        Container numPadContainer = new Container();
+        numPadContainer.setLayout(new GridLayout(0,3));
+        this.generateNumpadButtons().forEach(numPadContainer::add);
+        frame.add(numPadContainer);
 
 
         // Make the frame visible
